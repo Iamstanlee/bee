27398888656f4239a117c59509cf4c5c53b9eb0f
Stanley Akpama <stanlee@0x09.local>
2020-12-03 13:57:54 -0800

    #fixed install.sh issue

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo su
-install all dependencies
+# sudo su
+# install all dependencies
 sudo apt-get install golang;
 sudo apt-get install python3;
 sudo apt-get install python3-pip;
@@ -33,15 +33,15 @@ git clone https://github.com/maurosoria/dirsearch.git $dir/dirsearch;
 git clone https://github.com/projectdiscovery/nuclei.git $dir/nuclei; cd $dir/nuclei/cmd/nuclei/; go build;mv nuclei $dir
 git clone https://github.com/projectdiscovery/nuclei-templates $dir/nuclei-templates;
 git clone https://github.com/defparam/smuggler $dir/smuggler;
-git clone https://github.com/FortyNorthSecurity/EyeWitness.git $dir/eyeWitness
-chmod +x $dir/eyeWitness/Python/setup/setup.sh
-bash $dir/eyeWitness/Python/setup/setup.sh
+# git clone https://github.com/FortyNorthSecurity/EyeWitness.git $dir/eyeWitness
+# chmod +x $dir/eyeWitness/Python/setup/setup.sh
+# bash $dir/eyeWitness/Python/setup/setup.sh
 
 git clone https://github.com/1ndianl33t/Gf-Patterns;
 
 
-git clone https://github.com/devanshbatham/FavFreak $dir/favFreak;
-pip install -r $dir/favFreak/requirements.txt
+# git clone https://github.com/devanshbatham/FavFreak $dir/favFreak;
+# pip install -r $dir/favFreak/requirements.txt
 
 git clone https://github.com/aboul3la/Sublist3r.git $dir/sublist3r;
 pip install -r $dir/sublist3r/requirements.txt
@@ -52,7 +52,7 @@ pip install -r $dir/secretFinder/requirements.txt
 
 # organize, clean up, download wordlists and misc
 mkdir ~/.gf;
-wget https://raw.githubusercontent.com/Mad-robot/recon-tools/master/dicc.txt -o wordlist/directory.txt;
+wget https://raw.githubusercontent.com/Mad-robot/recon-tools/master/dicc.txt -o ~/wordlist/directory.txt;
 wget https://raw.githubusercontent.com/devanshbatham/ParamSpider/master/gf_profiles/potential.json -o ~/.gf/potential.json;
 mv $dir/sublist3r/Sublist3r.py $dir/sublist3r/sublist3r.py;
 mv $dir/secretFinder/SecretFinder.py $dir/secretFinder/secretFinder.py;
