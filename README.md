09268fd08ce9f83272f0768e4bd2a3fdbe79bdb0
Stanley Akpama <stanlee@0x09.local>
2020-12-05 11:33:34 -0800

    #fixed install.sh go installation

--- README.md
+++ README.md
@@ -13,35 +13,33 @@ Bee is a c-tier recon framework, inspired by Harsh-Bothra's [Bheem](https://gith
 
 ### Installation
 
--  clone this repo 
-	```
-	git clone https://github.com/Iamstanlee/bee.git
-	```
--  ```
-	cd bee;chmod +x install.sh
-	```
--  then run 
-	```
-	./install.sh
-	```
+- clone this repo
+  ```
+  git clone https://github.com/Iamstanlee/bee.git
+  cd bee;chmod +x install.sh
+  ```
+- then run
+  ```
+  ./install.sh
+  ```
 
 ### Usage
 
--  I've categorized the recon into two
-   -  small scope recon eg recon on _example.com_
-   -  medium scope recon eg _\*.example.com_
--  Either (not both) of the recon category can be run at a time
--  Create a new file _newtargets.txt_ to contain either small and medium scope targets
--  Save and run `bee`
--  For small scope, pass an 's' flag to tell bee it's small scope ie `bee -s`
+- I've categorized the recon into two
+  - small scope recon eg recon on _example.com_
+  - medium scope recon eg _\*.example.com_
+- Either (not both) of the recon category can be run at a time
+- Create a new file _newtargets.txt_ to contain either small and medium scope targets
+- Save and run `bee`
+- For small scope, pass an 's' flag to tell bee it's small scope ie `bee -s`
 
 ### Tools Used
 
--  gf
--  nuclei
--  subfinder
--  assetfinder
--  sublist3r
--  httpx
--  gau
--  waybackurls
+- gf
+- nuclei
+- subfinder
+- assetfinder
+- sublist3r
+- httpx
+- gau
+- waybackurls
--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-# sudo su
-# install all dependencies
-sudo apt-get install golang;
-sudo apt-get install python3;
-sudo apt-get install python3-pip;
-sudo apt-get install ruby;
-sudo apt-get install screen;
-sudo apt-get install git;
-wget https://golang.org/dl/go1.15.1.linux-amd64.tar.gz; 
-tar -C /usr/local -xzf go1.15.1.linux-amd64.tar.gz;
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc;source ~/.bashrc;
-
-pip install requests;
+# install apache2, ngrok
 mkdir ~/recon;
 mkdir ~/wordlist;
 dir=/usr/local/bin;
+
+# install all dependencies
+wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz;
+tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz;
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc;
+source ~/.bashrc;
+sudo apt-get install python3;
+sudo apt-get install ruby;
+sudo apt-get install screen;
+sudo apt-get install git;
+sudo apt-get install jq;
+pip3 install requests;
 
 # install go dependencies
 go get -u github.com/tomnomnom/assetfinder;
@@ -25,27 +25,15 @@ go get github.com/ffuf/ffuf;
 go get -u github.com/tomnomnom/gf;
 GO111MODULE=on go get -u -v github.com/lc/gau;
 GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx;
-# GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei;
 
 # install dependencies via git
-
 git clone https://github.com/maurosoria/dirsearch.git $dir/dirsearch;
-git clone https://github.com/projectdiscovery/nuclei.git $dir/nuclei; cd $dir/nuclei/cmd/nuclei/; go build;mv nuclei $dir; cd;
+git clone https://github.com/projectdiscovery/nuclei.git $dir/nuclei; cd $dir/nuclei/cmd/nuclei/; go build;mv nuclei $dir; cd -;
 git clone https://github.com/projectdiscovery/nuclei-templates $dir/nuclei-templates;
 git clone https://github.com/defparam/smuggler $dir/smuggler;
-# git clone https://github.com/FortyNorthSecurity/EyeWitness.git $dir/eyeWitness
-# chmod +x $dir/eyeWitness/Python/setup/setup.sh
-# bash $dir/eyeWitness/Python/setup/setup.sh
-
 git clone https://github.com/1ndianl33t/Gf-Patterns;
-
-
-# git clone https://github.com/devanshbatham/FavFreak $dir/favFreak;
-# pip install -r $dir/favFreak/requirements.txt
-
 git clone https://github.com/aboul3la/Sublist3r.git $dir/sublist3r;
 pip install -r $dir/sublist3r/requirements.txt
-
 git clone https://github.com/m4ll0k/SecretFinder $dir/secretFinder;
 pip install -r $dir/secretFinder/requirements.txt
 
@@ -56,7 +44,7 @@ wget https://raw.githubusercontent.com/Mad-robot/recon-tools/master/dicc.txt -o 
 wget https://raw.githubusercontent.com/devanshbatham/ParamSpider/master/gf_profiles/potential.json -o ~/.gf/potential.json;
 mv $dir/sublist3r/Sublist3r.py $dir/sublist3r/sublist3r.py;
 mv $dir/secretFinder/SecretFinder.py $dir/secretFinder/secretFinder.py;
-mv ~/home/go/bin/SubOver ~/home/go/bin/subOver;
+# mv ~/home/go/bin/SubOver ~/home/go/bin/subOver;
 mv Gf-Patterns/*.json ~/.gf;
 rm -rf Gf-Patterns;
 echo 'source $HOME/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc;
